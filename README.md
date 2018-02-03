9b9b76ebaaf28443da35d61a6465d4efed0a39d0
hyl <15921052117@163.com>
2018-02-03 15:25:33 +0800

    修改readme pom新增tomcat编码的配置 User类测试修改

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# cweb
-a javaEE project
+# cweb j2EE project
+
+[TOC]
+
+#学习资料
+
+##SSM框架相关
+1. [@responseBody注解的使用](https://www.cnblogs.com/qiankun-site/p/5774325.html)
+2. [@RequestParam的作用](http://blog.csdn.net/u012726702/article/details/51813235)
+3. [SSM框架下各个层的解释说明](http://blog.csdn.net/lutianfeiml/article/details/51864160)
+
+## 数据库相关
+1. [java与mysql时间类型对应](http://blog.csdn.net/xinghuo0007/article/details/51500923)
+
+
+#项目修改注意事项
+1. .xml文件和.java文件修改后，需要在maven里先执行clean再执行install，这之后再启动服务器才能完成修改
+2. .properties和前端修代码修改后,直接重启服务器即可。
+
+
--- cweb-model/src/main/java/com/test/cweb/model/User.java
+++ cweb-model/src/main/java/com/test/cweb/model/User.java
@@ -1,5 +1,7 @@
 package com.test.cweb.model;
 
+import java.sql.Timestamp;
+
 /**
  * @author caiminjie
  * @date 18/2/1
@@ -8,6 +10,14 @@ public class User {
     private int id;
     private String name;
     private String password;
+    private String account;
+    private String phoneNumber;
+    private String Email;
+    private Timestamp createTime;
+    private Timestamp loginTime;
+    private Timestamp lastLoginTime;
+    private int loginCount;
+    private String ip;
 
     public int getId() {
         return id;
@@ -31,5 +41,69 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public Timestamp getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Timestamp createTime) {
+        this.createTime = createTime;
+    }
+
+    public Timestamp getLoginTime() {
+        return loginTime;
+    }
+
+    public void setLoginTime(Timestamp loginTime) {
+        this.loginTime = loginTime;
+    }
+
+    public Timestamp getLastLoginTime() {
+        return lastLoginTime;
+    }
+
+    public void setLastLoginTime(Timestamp lastLoginTime) {
+        this.lastLoginTime = lastLoginTime;
+    }
+
+    public int getLoginCount() {
+        return loginCount;
+    }
+
+    public void setLoginCount(int loginCount) {
+        this.loginCount = loginCount;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
     }
 }
